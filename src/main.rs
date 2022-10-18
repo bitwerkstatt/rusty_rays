8dff8bd64f40302414420221f0346633ef6926ae
Thomas Manthey <mail@thomas-manthey.de>
2022-10-18 19:37:35 +0200

    Removed Coordinate, presumably overhead.

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod maths;
+
+fn main() {
+    println!("Mommy, when I grow up I want to be a raytracer!");
+}
--- src/maths.rs
+++ src/maths.rs
@@ -1,40 +1,9 @@
 use std::f32::EPSILON;
 
-pub struct Coordinate {
-    value: f32
-}
-
-impl Coordinate {
-
-    pub fn new(value: f32) -> Coordinate {
-        Self {
-            value
-        }
-    }
-
-    pub fn zero() -> Coordinate {
-        Self {
-            value: 0.0
-        }
-    }
-
-    pub fn eq(&self, other: &Coordinate) -> bool {
-        (self.value - other.value) <= f32::EPSILON
-    }
-
-    pub fn add(&self, other: &Coordinate) -> f32 {
-        self.value + other.value
-    }
-
-    pub fn sub(&self, other: &Coordinate) -> f32 {
-        self.value - other.value
-    }
-}
-
 pub struct Tuple {
-    x: Coordinate,
-    y: Coordinate,
-    z: Coordinate,
+    x: f32,
+    y: f32,
+    z: f32,
     w: f32
 }
 
@@ -42,18 +11,18 @@ impl Tuple {
 
     pub fn new(x: f32, y:f32, z:f32, w: f32) -> Tuple {
         Self {
-            x: Coordinate::new(x),
-            y: Coordinate::new(y),
-            z: Coordinate::new(z),
+            x,
+            y,
+            z,
             w
         }
     }
 
     pub fn zero() -> Tuple {
-        Self {
-            x: Coordinate::new(0.0),
-            y: Coordinate::new(0.0),
-            z: Coordinate::new(0.0),
+        Tuple {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
             w: 1.0
         }
     }
@@ -76,9 +45,9 @@ mod tests {
     #[test]
     fn test_point() {
         let a = Tuple::new(4.3, -4.2, 3.1, 1.0 );
-        assert_eq!(a.x.value, 4.3);
-        assert_eq!(a.y.value, -4.2);
-        assert_eq!(a.z.value, 3.1);
+        assert_eq!(a.x, 4.3);
+        assert_eq!(a.y, -4.2);
+        assert_eq!(a.z, 3.1);
         assert!(a.is_point());
         assert!(!a.is_vector());
     }
@@ -86,9 +55,9 @@ mod tests {
     #[test]
     fn test_vector() {
         let a = Tuple::new(4.3, -4.2, 3.1, 0.0 );
-        assert_eq!(a.x.value, 4.3);
-        assert_eq!(a.y.value, -4.2);
-        assert_eq!(a.z.value, 3.1);
+        assert_eq!(a.x, 4.3);
+        assert_eq!(a.y, -4.2);
+        assert_eq!(a.z, 3.1);
         assert!(!a.is_point());
         assert!(a.is_vector());
     }
